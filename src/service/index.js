98af883cc01a3a1204f8d79b0aa6cf9f9147d39e
juniorpb <juniorfoxld@hotmail.com>
2020-02-25 20:48:49 -0300

    service return

--- src/service/index.js
+++ src/service/index.js
@@ -3,16 +3,13 @@ const axios = require('axios');
 async function getCidadesByEstado(siglaEstado) {
     
     const url = `http://educacao.dadosabertosbr.com/api/cidades/${siglaEstado}`;
-    let response = "";
 
     try {
-        response = await axios.get(url);
+        return await axios.get(url);
         
     } catch (error) {
-        response = "Erro ao buscar cidades";
+        return "Erro ao buscar cidades";
     }
-    console.log(response.data);
-    // return response;
 }
 
 module.exports = getCidadesByEstado;

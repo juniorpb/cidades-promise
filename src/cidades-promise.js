89f306c3b00098a4744779a58c9f7a9000521156
juniorpb <juniorfoxld@hotmail.com>
2020-02-25 21:54:44 -0300

    limpado dado retornado do INEP

--- src/cidades-promise.js
+++ src/cidades-promise.js
@@ -1,10 +1,28 @@
 const getCidades = require('./service');
 
 async function getCidadesEstado(siglaEstado){
-     const response =  await getCidades(siglaEstado)
 
-     return response.data;
+    if (siglaEstado.length !== 2) {
+        return JSON.stringify({"erro": "Sigla do estado estar incorreta"});
     }
 
+    const listaDeCidades   = await getCidades(siglaEstado)
+    const respostaTratada  = await trataResposta(listaDeCidades);
+
+    return respostaTratada;
+}
+
+/* Trata a resposta que vem da API do INEP
+ * Limpamos os dados deixando apenas os nomes das Cidades
+ */
+function trataResposta(res) {
+    const cidades = res.data.reduce((dadoPuro, linhaCidade) =>{
+        const cidade = linhaCidade.split(":")
+        return [...dadoPuro, cidade[1]]
+
+    }, [])
+
+    return cidades;
+} 
 
 module.exports = getCidadesEstado;

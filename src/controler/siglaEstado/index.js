67ac98bfbd8e2d6066a049fd374e048d8332e6ca
juniorpb <juniorfoxld@hotmail.com>
2020-02-26 23:00:55 -0300

    create components modulos

---
+++ src/controler/siglaEstado/index.js
@@ -0,0 +1,28 @@
+const getCidades = require('../../service/siglaEstado');
+
+async function getCidadesEstado(siglaEstado){
+
+    if (siglaEstado.length !== 2) {
+        return JSON.stringify({"erro": "Sigla do estado estar incorreta"});
+    }
+
+    const listaDeCidades   = await getCidades(siglaEstado)
+    const respostaTratada  = await trataResposta(listaDeCidades);
+
+    return respostaTratada;
+}
+
+/* Trata a resposta que vem da API do INEP
+ * Limpamos os dados deixando apenas os nomes das Cidades
+ */
+function trataResposta(res) {
+    const cidades = res.data.reduce((dadoPuro, linhaCidade) =>{
+        const cidade = linhaCidade.split(":")
+        return [...dadoPuro, cidade[1]]
+
+    }, [])
+
+    return cidades;
+} 
+
+module.exports = getCidadesEstado;

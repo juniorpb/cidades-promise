f1d94ddbc6bc8e81fe393d07721a29506e1d1704
juniorpb <junior.cgjr@gmail.com>
2023-04-10 00:51:54 -0300

    fix: get cities by DDD

--- src/controler/ddd/index.js
+++ src/controler/ddd/index.js
@@ -1,42 +1,38 @@
-const getCidades = require('../../service/ddd');
+const getCidades = require("../../service/ddd");
 
-async function getCidadesByDdd(ddd){
+async function getCidadesByDdd(ddd) {
+  ddd = ddd.toString();
 
-    ddd = ddd.toString();
+  if (ddd.length !== 2) {
+    return JSON.stringify({ error: "DDD incorreto." });
+  }
 
-    if (ddd.length !== 2) {
-        return JSON.stringify({"error": "DDD incorreto."});
-    }
+  try {
+    const listaDeCidades = await getCidades(ddd);
+    const respostaTratada = await trataResposta(listaDeCidades.data);
 
-    try {
-        const listaDeCidades   = await getCidades(ddd);
-        const respostaTratada  = await trataResposta(listaDeCidades.data.payload);
-
-        return respostaTratada;
-    
-    } catch (error) {
-        return JSON.stringify({"error":"DDD não existe."});
-    }
+    return respostaTratada;
+  } catch (error) {
+    return JSON.stringify({ error: "DDD não existe." });
+  }
 }
 
-/* Trata a resposta que vem da API 
+/* Trata a resposta que vem da API
  * Limpamos os dados deixando apenas os nomes das Cidades
  */
 function trataResposta(data) {
-    const body = {
-        estado : {},
-        cidades : {}
-    }
+  const body = {
+    estado: {},
+    cidades: {},
+  };
 
-    // UF do estado DDD
-    body.estado = data[0].estado;
+  // UF do estado DDD
+  body.estado = data.state;
 
-    // array com todas as cidades DDD
-    body.cidades = data.reduce((dadoPuro, linhaCidade) =>{
-        return [...dadoPuro, linhaCidade.cidade];
-    }, []);
+  // array com todas as cidades DDD
+  body.cidades = data.cities;
 
-    return body;
-} 
+  return body;
+}
 
 module.exports = getCidadesByDdd;
--- src/service/state/index.js
+++ src/service/state/index.js
@@ -1,15 +1,13 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function getCidadesByEstado(siglaEstado) {
-    
-    const url = `http://educacao.dadosabertosbr.com/api/cidades/${siglaEstado}`;
+  const url = `http://educacao.dadosabertosbr.com/api/cidades/${siglaEstado}`;
 
-    try {
-        return await axios.get(url);
-        
-    } catch (error) {
-        return JSON.stringify({"erro": "Erro ao buscar cidades"});
-    }
+  try {
+    return await axios.get(url);
+  } catch (error) {
+    return JSON.stringify({ erro: "Erro ao buscar cidades" });
+  }
 }
 
 module.exports = getCidadesByEstado;
